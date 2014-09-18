0615d4fd8405d432e58cab7ad7e895b5a4eaeaed
Bryan Guerre <bguerre12@gmail.com>
2014-09-17 17:45:20 -0700

    Initial Structure Release
    This is the initial structure of the code. Given to us by the
    instructor, no need for any more functions or files.

---
+++ TicTacToe/TicTacToe/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream> 
+#include <cmath> 
+#include <cstdlib>
+
+using namespace std;
+const int BOARD_SIZE = 3;
+
+int main(int argc, char* argv[]) {
+	//1. Print out the board state
+	
+	//2. Ask for move 
+
+	//3. Verify the move
+
+	//4. Update the game
+}
+
+void PrintBoard(char board[BOARD_SIZE][BOARD_SIZE]) {
+
+}
+
+void GetMove(int *row, int *col) {
+
+}
+
+bool MoveIsValid(char board[BOARD_SIZE][BOARD_SIZE], int row, int col) {
+
+}
