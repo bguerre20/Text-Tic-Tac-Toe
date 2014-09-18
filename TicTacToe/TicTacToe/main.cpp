1cc0c2f1b8548f5edac1daa07ca0c61c3439457e
Bryan Guerre <bguerre12@gmail.com>
2014-09-17 18:40:31 -0700

    Get Move Function
    1st Draft of get move function completed, also added 9 turn game loop to
    determine what to print out via mod operator.

--- TicTacToe/TicTacToe/main.cpp
+++ TicTacToe/TicTacToe/main.cpp
@@ -7,15 +7,25 @@ using namespace std;
 const int BOARD_SIZE = 3;
 
 int main(int argc, char* argv[]) {
-	//1. Print out the board state
-	int board[BOARD_SIZE][BOARD_SIZE] = {0};
-	PrintBoard(board);
-	//2. Ask for move 
-	int row = 0, col = 0;
-	GetMove(&row, &col);
-	//3. Verify the move
+	for(int i = 1; i < 9; i++) {
 
-	//4. Update the game
+		//1. Print out the board state
+		int board[BOARD_SIZE][BOARD_SIZE] = {0};
+		PrintBoard(board);
+		//2. Ask for move 
+		int row = 0, col = 0;
+
+		if(i%2 == 1)
+			cout<<"X's turn: ";
+		else
+			cout<<"O's turn: ";
+		GetMove(&row, &col);
+
+		cout<<"row: "<< row<<", col: "<<col<<endl;//testing purposes
+		//3. Verify the move
+
+		//4. Update the game
+	}
 }
 
 //Print out the first line, then nested loop to print out each board spot, leading 
@@ -41,8 +51,10 @@ void PrintBoard(int board[BOARD_SIZE][BOARD_SIZE]) {
 	cout<<endl;
 }
 
+//Create a junke variable for comma, like suggested, get input from user. win.
 void GetMove(int *row, int *col) {
-	
+	char junk = '.';
+	cin>>*row>>junk>>*col;
 }
 
 bool MoveIsValid(char board[BOARD_SIZE][BOARD_SIZE], int row, int col) {
